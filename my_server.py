6c33352ae24f646fe7dbf434b9d0d697055f8779
Vadim Akulov <explosive.va@gmail.com>
2021-10-04 21:54:53 +0300

    Добавдены try except

--- my_server.py
+++ my_server.py
@@ -3,7 +3,6 @@ import psycopg2
 from config import host, user, password, db_name
 
 
-
 def validation(command, ready_data):
     request_answer = 'АМОЖНА? РКСОК/1.0\r\n'
     request_body = command + ' ' + ready_data[0] + ' ' + 'РКСОК/1.0\r\n'
@@ -73,7 +72,7 @@ def ZAPISHI(ready_data):
         )
 
         connection.autocommit = True
-        
+
     # insert data into table
         with connection.cursor() as cursor:
             cursor.execute(
@@ -162,34 +161,49 @@ async def serve_client(reader, writer):  # ОБРАБОТЧИК КЛИЕНТА
     data_lines = message.split('\r\n')  # Создаем список из всех строк
     # В переменной сохраняем основной полный запрос от клиента
     header = data_lines[0].split(' ')
-    # Разбиваем запрос на команду и врсию протокола в переменные
-    command, protocol_version = header.pop(0), header.pop(-1)
-    # Сохраняем в переменную текст пользователя из запроса
-    user_text = ' '.join(header)
-    body_phone = [num for num in data_lines[1:]
-                  if num]  # Создаем список телефонов
-    # Создаем список с именем и списком телефонов
-    ready_data = [user_text, body_phone]
 
-    # ПРОВЕРЯЕМ ПРАВИЛЬНОСТЬ ЗАПРОСА, выполняем команды and send response
-    if protocol_version == 'РКСОК/1.0':
-        if command in COMMAND_TO_ACTION:
-            if user_text:
-                if validation(command, ready_data) == "МОЖНА РКСОК/1.0":
-                    print('[INFO serv] Ответ от сервера проверки:\r\nМОЖНА РКСОК/1.0')         
-                    result = COMMAND_TO_ACTION[command](ready_data)
-                    writer.write(result.encode())
+    try:
+        # Разбиваем запрос на команду и врсию протокола в переменные
+        command, protocol_version = header.pop(0), header.pop(-1)
+        # Сохраняем в переменную текст пользователя из запроса
+        user_text = ' '.join(header)
+        # Создаем список телефонов
+        body_phone = [num for num in data_lines[1:] if num]
+        # Создаем список с именем и списком телефонов
+        ready_data = [user_text, body_phone]
+
+        # ПРОВЕРЯЕМ ПРАВИЛЬНОСТЬ ЗАПРОСА, выполняем команды and send response
+        if protocol_version == 'РКСОК/1.0':
+            if command in COMMAND_TO_ACTION:
+                if user_text:
+                    if validation(command, ready_data) == "МОЖНА РКСОК/1.0":
+                        print(
+                            '[INFO serv] Ответ от сервера проверки:\r\nМОЖНА РКСОК/1.0')
+                        result = COMMAND_TO_ACTION[command](ready_data)
+                        writer.write(result.encode())
+                        await writer.drain()
+                else:
+                    writer.write('НИПОНЯЛ РКСОК/1.0'.encode())
                     await writer.drain()
             else:
+                print[f'[INFO serv] ~{command}~ Unknown server command']
+        else:
+            try:
+                print[f'[INFO serv] ~{protocol_version}~ Unknown protocol command']
+            except TypeError as _ex:
+                print(f'Твой запрос не правильный дружок:', _ex)
                 writer.write('НИПОНЯЛ РКСОК/1.0'.encode())
                 await writer.drain()
-        else:
-            print[f'[INFO serv] ~{command}~ Unknown server command']
-    else:
-        print[f'[INFO serv] ~{protocol_version}~ Unknown protocol command']
 
-    print("[INFO serv] Close the connection")
-    writer.close()
+        print("[INFO serv] Close the connection")
+        writer.close()
+
+    except IndexError as _ex:
+        print(f'Твой запрос не правильный дружок:', _ex)
+        writer.write('НИПОНЯЛ РКСОК/1.0'.encode())
+        await writer.drain()
+        print("[INFO serv] Close the connection")
+        writer.close()
 
 
 if __name__ == '__main__':
